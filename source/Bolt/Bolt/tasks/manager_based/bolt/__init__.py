f99845e78109af7eef65aeddc2b5f8e8ed554818
MAZHIPENG001 <1246896046@qq.com>
2026-08-19 10:55:37 +0800

    remove depth

--- source/Bolt/Bolt/tasks/manager_based/bolt/__init__.py
+++ source/Bolt/Bolt/tasks/manager_based/bolt/__init__.py
@@ -33,12 +33,3 @@ gym.register(
 )
 
 
-gym.register(
-    id="Bolt-Soccer-Depth-v0",
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": f"{__name__}.depth_env_cfg:BoltDepthEnvCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
-    },
-)
